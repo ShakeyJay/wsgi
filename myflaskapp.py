45c757f547ff5122d339aabe5c11e0ad8f6e3414
Josh Hall <shakeyjaystudios@gmail.com>
2019-07-08 07:38:57 -0400

    Semi-working locally

--- myflaskapp.py
+++ myflaskapp.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 # Probably want to set this up to just be griffin/ with no port number so
 # that all of our main stuff goes there. This is where you login and then it
 # take you to home page where you can go to all of the other pages needed.
-app.config["SERVER_NAME"] = 'localhost:3031'
+# app.config["SERVER_NAME"] = 'localhost:3031'
 
 @app.route('/')
 def index():
